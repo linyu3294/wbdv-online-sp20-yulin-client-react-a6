5fdcf23b22b563441d4fdb8f11629894dae17fb9
Yu Lin <linyu3294@gmail.com>
2020-02-21 22:22:39 -0500

    changed urls

--- src/services/WidgetService.js
+++ src/services/WidgetService.js
@@ -1,9 +1,9 @@
 export const findWidgetsForTopic = async (topic) =>
-    await fetch(`http://localhost:8080/api/topics/${topic}/widgets/`)
+    await fetch(`https://calm-bastion-52680.herokuapp.com/api/topics/${topic}/widgets/`)
         .then(response => response.json())
 
 export const updateWidget = (wid, widget) =>
-    fetch(`http://localhost:8080/api/widgets/${wid}`, {
+    fetch(`https://calm-bastion-52680.herokuapp.com/api/widgets/${wid}`, {
         method: "PUT",
         body: JSON.stringify(widget),
         headers: {
@@ -12,16 +12,16 @@ export const updateWidget = (wid, widget) =>
     }).then(response => response.json())
 
 export const findAllWidgets = () =>
-    fetch("http://localhost:8080/api/widgets")
+    fetch("https://calm-bastion-52680.herokuapp.com/api/widgets")
         .then(response => response.json())
 
 export const deleteWidget = (widgetId) =>
-    fetch(`http://localhost:8080/api/widgets/${widgetId}`, {
+    fetch(`https://calm-bastion-52680.herokuapp.com/api/widgets/${widgetId}`, {
         method: "DELETE"
     }).then(response => response.json())
 
 export const createWidget = (topicId, widget) =>
-    fetch("http://localhost:8080/api/widgets", {
+    fetch("https://calm-bastion-52680.herokuapp.com/api/widgets", {
         method: "POST",
         body: JSON.stringify(widget),
         headers: {
